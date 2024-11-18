b4555e12afe50419fbdbe2fa94f082b88cc4464b
chuu ex loona (otavio) <otavio54tencaa@gmail.com>
2024-11-18 09:03:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'K-pop GGs',
+    'Qual é a idol mais famosa do kpop?',
+    'Lisa (BLACKPINK)'
+)
+
+criaCartao(
+    'POP',
+    'Quem é a rainha do pop?',
+    'Madonna'
+)
+
+criaCartao(
+    'Rap',
+    'Quem é a rainha do rap?',
+    'Nick Minaj'
+)
+
+criaCartao(
+    'Música',
+    'Qual é o album do ano de 2024?',
+    'BRAT (Charli XCX)'
+)
